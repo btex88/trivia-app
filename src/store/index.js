6c36858d8b78c34657ca005b4cb735fbbe0e3efe
Bruno Teixeira <btex88@gmail.com>
2022-01-19 11:35:52 -0300

    fix(store): changed to default redux dev tools and removes cypress conditional in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-if (window.Cypress) {
-  window.store = store;
-}
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
